f517d56720528e25fab27e476709029cf593413c
hj1933 <1461024580@qq.com>
2020-01-06 18:56:39 +0800

    修改 docker.sh，新增 webssh 启动

--- docker.sh
+++ docker.sh
@@ -14,22 +14,25 @@ checkSudo
 
 # 启用 Python 虚拟环境
 source /root/.virtualenvs/auto_deploy/bin/activate
-pip install docker-compose -i https://pypi.douban.com/simple
+# pip install docker-compose -i https://pypi.douban.com/simple
 
 # 根据输入参数调用相应命令，操作 docker 容器
 if [ $# -eq 1 ]
 then
 	case $1 in
 		"run")
-			docker-compose up;;
+			docker-compose up
+			supervisorctl start wssh;;
 		"restart")
-			docker-compose restart;;
+			docker-compose restart
+			supervisorctl restart wssh;;
 		"drun")			
 			docker-compose up -d;;
 		"logs")
 			docker-compose logs;;
 		"stop")
-			docker-compose down;;
+			docker-compose down
+			supervisorctl stop wssh;;
 		*)
 			echo -e "Usage sudo ./docker.sh {run|restart|drun|logs|stop}"
 	esac
